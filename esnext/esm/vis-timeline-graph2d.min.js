43b371cbfb6b00dc2821c86b2898ef94de518512
Andrei Corpodeanu <andrei.corpodeanu@gmail.com>
2024-10-03 18:07:41 +0300

    fix: push declarations to repo

--- esnext/esm/vis-timeline-graph2d.min.js
+++ esnext/esm/vis-timeline-graph2d.min.js
@@ -5,7 +5,7 @@
  * Create a fully customizable, interactive timeline with items and ranges.
  *
  * @version 0.0.1
- * @date    2024-09-30T13:13:31.950Z
+ * @date    2024-10-03T15:06:18.301Z
  *
  * @copyright (c) 2011-2017 Almende B.V, http://almende.com
  * @copyright (c) 2017-2019 visjs contributors, https://github.com/visjs
